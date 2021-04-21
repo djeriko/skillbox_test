afebc35ad6c0a6e4cb8017a66c1c7de6a15987ab
djeriko <jerikosalivan@gmail.com>
2021-04-21 23:20:02 +0300

    initial commit

---
+++ lib/blocs/bloc/tabs_event.dart
@@ -0,0 +1,4 @@
+part of 'tabs_bloc.dart';
+
+@immutable
+abstract class TabsEvent {}
---
+++ lib/blocs/bloc/tabs_state.dart
@@ -0,0 +1,6 @@
+part of 'tabs_bloc.dart';
+
+@immutable
+abstract class TabsState {}
+
+class TabsInitial extends TabsState {}
